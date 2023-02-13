52ae3657fc88abd832171f233b512b56994b2bd5
bths-ax <ardenx@nycstudents.net>
2023-02-13 07:55:25 -0500

    utility method oh well

--- src/ArrayListAlgorithms.java
+++ src/ArrayListAlgorithms.java
@@ -1,22 +1,32 @@
 import java.util.ArrayList;
 
 public class ArrayListAlgorithms {
+	public static boolean isGreater(Student a, Student b) {
+		int lastCmp = a.getLastName().compareTo(b.getLastName());
+		if (lastCmp > 0) return true;
+		if (lastCmp < 0) return false;
+		int firstCmp = a.getFirstName().compareTo(b.getFirstName());
+		if (firstCmp > 0) return true;
+		if (firstCmp < 0) return false;
+		return a.getGpa() < b.getGpa();
+	}
+
 	public static void sortStudents(ArrayList<Student> toSort) {
 		// inefficient sort bc its easier to write and no constraints were given
 		for (int i = 0; i < toSort.size(); i++) {
+			int k = i;
+			Student iStud = toSort.get(i);
+			Student kStud = toSort.get(i);
 			for (int j = i + 1; j < toSort.size(); j++) {
-				int k = j - 1;
-				Student kStud = toSort.get(k);
 				Student jStud = toSort.get(j);
-				if (
-					kStud.getLastName().compareTo(jStud.getLastName()) >= 0 &&
-					kStud.getFirstName().compareTo(jStud.getFirstName()) >= 0 &&
-					kStud.getGpa() > jStud.getGpa()
-				) {
-					toSort.set(k, jStud);
-					toSort.set(j, kStud);
+				if (isGreater(kStud, jStud)) {
+					kStud = jStud;
+					k = j;
 				}
 			}
+			toSort.set(i, kStud);
+			toSort.set(k, iStud);
+			System.out.println("Swapping " + i + " and " + k);
 		}
 	}
 }
